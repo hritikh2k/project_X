0c781679d225ae9e1ebe897b11efccd1ab74baf2
Hritik kumar <hritikkumar2011@gmail.com>
2024-08-15 22:52:06 +0530

    initial commit

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 export default function generateTokenAndSetCookie(newUser, res) {
     const token = jwt.sign(newUser, process.env.JWT_SECRET, {
         expiresIn: 15 * 24 * 60 * 60 * 1000,
-        httpOnly: true
+        httpOnly: true,
+
     })
 }
