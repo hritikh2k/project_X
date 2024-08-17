414c2ecc0ade7497bb7c23afa1129d7ca7761982
Hritik kumar <hritikkumar2011@gmail.com>
2024-08-18 00:54:42 +0530

    intial commit

--- backend/controllers/users.controllers.js
+++ backend/controllers/users.controllers.js
@@ -3,7 +3,21 @@ import User from "../models/user.model.js";
 import bycrypt from "bcryptjs";
 
 export const getUserProfile = async (req, res) => {
-
+    const { username } = req.params;
+    try {
+        const user = await User.findOne({ username }).select("-password")
+        if (!user) {
+            res.status(400).json({
+                error: "User not found"
+            })
+        }
+        res.status(200).json(user)
+    } catch (error) {
+        console.log(`Error in getUserProfile controll ${error.message}`);
+        res.status(500).json({
+            error: "Internal server error from getUserProfile users.controller"
+        })
+    }
 }
 
 export const followUnfollow = async (req, res) => {
@@ -13,4 +27,3 @@ export const followUnfollow = async (req, res) => {
 export const updateUserProfile = async (req, res) => {
 
 }
-
--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { protectedRoute } from "../middleware/protectedRoute.js";
+import { getUserProfile, followUnfollow, updateUserProfile } from "../controllers/users.controllers.js";
+
 const router = express.Router();
 
 router.get('/profile/:username', protectedRoute, getUserProfile);
